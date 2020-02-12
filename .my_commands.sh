9aa683dfff681d928954e4990051f60f7ce861b0
Ethan Francis <basix317@gmail.com>
2020-02-12 15:51:44 -0500

    function add DIR

--- .my_commands.sh
+++ .my_commands.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/env bash
-
+#! /usr/bin/env python3
 
 function create() {
-   python3 create.py
-    echo $1   
+    cd Desktop/code/automation/auto_nav_test 
+    python3 create.py 
+    echo $1
+
 }
-
-
