a748e742c61b93f13b58153e93a385625bc5bbba
Ethan Francis <basix317@gmail.com>
2020-02-12 17:33:41 -0500

    rm paranthesis from DIR

--- .my_commands.sh
+++ .my_commands.sh
@@ -1,11 +1,6 @@
 #! /usr/bin/env python3
 
 function create() {
-    cd Desktop/code/automation/auto_nav_test/ 
-       echo python3 create.py " $1" " $2"
-            
+    cd /Desktop/code/automation/auto_nav_test && python3 create.py  
 
 }
-
-
-function create "$arg1"  "$arg2"
