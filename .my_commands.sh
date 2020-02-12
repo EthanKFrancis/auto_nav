a6f02bf2fb339216f1be03689e1193ff6a726a05
Ethan Francis <basix317@gmail.com>
2020-02-12 17:17:03 -0500

    added values to arg1 and 2

--- .my_commands.sh
+++ .my_commands.sh
@@ -1,10 +1,12 @@
 #! /usr/bin/env python3
 
-function create() {
+create() {
     cd Desktop/code/automation/auto_nav_test 
         python3 create.py 
             echo $1
             echo $2
 
-    
 }
+
+
+create "$arg1"  "$arg2"
