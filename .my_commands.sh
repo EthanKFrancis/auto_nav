7df7434681a9a628b2d3fe5d3c2deb783ede2a36
Ethan Francis <basix317@gmail.com>
2020-02-12 17:22:18 -0500

    ...

--- .my_commands.sh
+++ .my_commands.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 function create() {
-    cd Desktop/code/automation/auto_nav_test 
+    cd Desktop/code/automation/auto_nav_test/ 
        echo python3 create.py " $1" " $2"
             
 
