c1d2ef729a7a18545a9fb80b94594b6f4a4257de
Ethan Francis <basix317@gmail.com>
2020-02-12 21:07:44 -0500

    commit 3 getting .py and parameter output

--- .mycommands.sh
+++ .mycommands.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-create() {
-    cd Desktop/code/automation/auto_nav_test
+function create() {
+    cd /home/nemesis/Desktop/code/automation/auto_nav_test
     python3 create.py
     echo $1
 
